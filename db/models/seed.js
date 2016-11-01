68ad99fde54e1ebe3b508fcac964b875036dc3b8
Jiheh Ritterling <jritterling@gmail.com>
2016-11-01 17:53:20 -0400

    seeds created

--- db/models/seed.js
+++ db/models/seed.js
@@ -1,13 +1,47 @@
-const db = require('APP/db')
+const db = require('APP/db');
 
 const seedUsers = () => db.Promise.map([
   {name: 'so many', email: 'god@example.com', password: '1234', roles: 'user'},
   {name: 'Barack Obama', email: 'barack@example.gov', password: '1234', roles: 'admin'},
-], user => db.model('users').create(user))
+], user => db.model('users').create(user));
+
+const seedProducts = () => db.Promise.map([
+		{name: 'Candy Store', price: 100, description: 'Buy some candy'},
+		{name: 'Candy Store', price: 100, description: 'Buy some candy'},
+		{name: 'Candy Store', price: 100, description: 'Buy some candy'}
+	], product => db.model('products').create(product));
+
+const seedCategories = () => db.Promise.map([
+	{name: 'beauty', image: 'https://placeholdit.imgix.net/~text?txtsize=33&txt=350%C3%97350&w=350&h=350'},
+	{name: 'sports', image: 'https://placeholdit.imgix.net/~text?txtsize=33&txt=350%C3%97350&w=350&h=350'},
+	{name: 'fashion', image: 'https://placeholdit.imgix.net/~text?txtsize=33&txt=350%C3%97350&w=350&h=350'},
+	{name: 'entertainment', image: 'https://placeholdit.imgix.net/~text?txtsize=33&txt=350%C3%97350&w=350&h=350'},
+	{name: 'technology', image: 'https://placeholdit.imgix.net/~text?txtsize=33&txt=350%C3%97350&w=350&h=350'},
+	{name: 'food', image: 'https://placeholdit.imgix.net/~text?txtsize=33&txt=350%C3%97350&w=350&h=350'}
+], category => db.model('categories').create(category));
+
+let createdCategories;
+
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
 db.didSync
   .then(() => db.sync({force: true}))
   .then(seedUsers)
   .then(users => console.log(`Seeded ${users.length} users OK`))
+  .then(seedCategories)
+  .then(categories => {
+  	createdCategories = categories;
+  	console.log(`Seeded ${categories.length} categories OK`);
+  })
+  .then(seedProducts)
+  .then(products => {
+  	createdProducts = products;
+  	return db.Promise.all(products.map(product => product.addCategory(createdCategories[getRandomIntInclusive(0, createdCategories.length)])));
+  })
+  .then(products => console.log(`Seeded ${products.length} products OK`))
   .catch(error => console.error(error))
-  .finally(() => db.close())
+  .finally(() => db.close());
