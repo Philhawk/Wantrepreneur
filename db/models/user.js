df7cae8a59c165bdb848ca15ef4ff8b847ba4107
Jiheh Ritterling <jritterling@gmail.com>
2016-11-01 14:30:45 -0400

    minor bug fixed

--- db/models/user.js
+++ db/models/user.js
@@ -48,12 +48,11 @@ function setEmailAndPassword(user) {
   if (!user.password) return Promise.resolve(user);
 
   return new Promise((resolve, reject) =>
-	  bcrypt.hash(user.get('password'), 10, (err, hash) => {
-		  if (err) reject(err);
-		  user.set('password_digest', hash);
-    resolve(user);
-    });
-  );
+    bcrypt.hash(user.get('password'), 10, (err, hash) => {
+      if (err) reject(err);
+      user.set('password_digest', hash);
+      resolve(user);
+    }));
 }
 
 module.exports = User;
