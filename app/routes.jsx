7ca019e7bb1b55b8eb71a65dc85d2c8ba0be5020
Philhawk <pkjacob@gmail.com>
2016-11-02 16:08:35 -0400

    fixed routes

--- app/routes.jsx
+++ app/routes.jsx
@@ -5,7 +5,7 @@ import {Route, IndexRoute,Redirect} from 'react-router';
 import Root from './components/RootContainer';
 import CategoriesComponent from './components/categories/Categories';
 import PriceComponent from './components/price/Price';
-import AllOptions from './components/all-options/AllOptions';
+import AllOptionsComponent from './components/all-options/AllOptions';
 
 const routes = (
   <Route path='/'>
