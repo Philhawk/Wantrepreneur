f54f30ae73b94bedbf5b1149a2bcedd055b14f72
Kate Humphrey <khumphrey7890@gmail.com>
2016-11-02 11:13:23 -0400

    price component added

---
+++ app/components/price/PriceContainer.jsx
@@ -0,0 +1,10 @@
+'use strict';
+
+import { connect } from 'react-redux';
+import Navbar from './NavbarComponent';
+
+const mapStateToProps = ({}) => ({
+
+});
+
+export default connect(mapStateToProps)(Navbar);
