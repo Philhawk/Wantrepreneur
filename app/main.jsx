554ad444469d4b38b91f487fdd9e1b173bf40df8
Emily H <emilyyho33@gmail.com>
2016-11-02 13:19:40 -0400

    IndexRoute as Root, Testing if is local issue

--- app/main.jsx
+++ app/main.jsx
@@ -1,14 +1,16 @@
-'use strict'
-import React from 'react'
-import {render} from 'react-dom'
-import { Provider } from 'react-redux'
+
+import React from 'react';
+import {render} from 'react-dom';
+import { Provider } from 'react-redux';
+import { Router, hashHistory, Route, IndexRoute} from 'react-router';
 
 import store from './store'
 import Root from './components/Root'
 
 render (
   <Provider store={store}>
-    <Root/>
+
+    <IndexRoute component={Root}/>
   </Provider>,
   document.getElementById('main')
 )
