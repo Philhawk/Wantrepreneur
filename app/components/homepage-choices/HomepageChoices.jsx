c876f3a67d2cc9ecfaab8b38a1c2453e97360316
Philhawk <pkjacob@gmail.com>
2016-11-03 17:23:51 -0400

    WIP beautify

--- app/components/homepage-choices/HomepageChoices.jsx
+++ app/components/homepage-choices/HomepageChoices.jsx
@@ -1,6 +1,7 @@
 import React from'react';
-import {Col, Row,Grid} from "react-bootstrap";
+import {Col, Row, Grid, Jumbotron} from "react-bootstrap";
 import {Link} from 'react-router';
+import AlarmIcon from 'react-material-icons/icons/action/alarm';
 
 
 export default class HomepageChoices extends React.Component {
@@ -13,12 +14,26 @@ export default class HomepageChoices extends React.Component {
 
 
       return (
-        <div>
+        <div className='homepage-choices'>
           <Grid>
-          <Row className="show-grid">
-            <Col md={6} mdPush={6}><code>&lt;{'Col md={6} mdPush={6}'} /&gt;</code></Col>
-            <Col md={6} mdPull={6}><code>&lt;{'Col md={6} mdPull={6}'} /&gt;</code></Col>
-          </Row>
+            <Row className="show-grid">
+              <Col md={6} mdPush={6} className='rightDivOpeningPage'>
+                <h1 className='heading'><span>Wantrepreneur</span></h1>
+                <h1 className='description-headline'>Help us choose the right business for you</h1>
+              </Col>
+              <Col md={6} mdPull={6} className='leftDivOpeningPage'>
+                <Col md={6} mdPush={6}>
+                  <Jumbotron className='boxes'>
+                    <AlarmIcon className='alarm'/>
+                  </Jumbotron>
+                </Col>
+                <Col md={6} mdPull={6}>
+                  <Jumbotron className='boxes'>
+                    Potato
+                  </Jumbotron>
+                </Col>
+              </Col>
+            </Row>
           </Grid>
         </div>
     );
