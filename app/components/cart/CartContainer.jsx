62515c1e5cab6c2255e9baa4e1649e782887fc8c
Anthony Ardito <anthonyardito@anthony-arditos-macbook-pro.local>
2016-11-03 23:37:42 -0400

    Removed extraneous SET_CART action

--- app/components/cart/CartContainer.jsx
+++ app/components/cart/CartContainer.jsx
@@ -2,7 +2,6 @@
 
 import Cart from './Cart';
 import { connect } from 'react-redux';
-import { setCart } from '../../reducers/cart';
 import { getCartFromLocal } from './CartHelpers';
 
 const mapStateToProps = ({ cart }, { params }) => ({
