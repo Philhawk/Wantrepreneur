ab325e409b16e9f693db01304abea76aeb6fc0dd
Emily H <emilyyho33@gmail.com>
2016-11-04 16:07:13 -0400

    shopping cart icon and toolbar added

--- app/components/navbar/Navbar.jsx
+++ app/components/navbar/Navbar.jsx
@@ -1,40 +1,68 @@
 
 import React from 'react';
-import { Nav, Navbar, NavItem, NavDropdown, MenuItem } from "react-bootstrap";
-import { AppBar, Tabs, Tab } from 'material-ui'
-import { Link } from 'react-router';
+// import {Nav, Navbar, NavItem, NavDropdown, MenuItem} from "react-bootstrap";
+import {AppBar, Tabs, Tab, Toolbar, ToolbarGroup, ToolbarTitle} from 'material-ui';
+import FontIcon from 'material-ui/FontIcon';
+import {Link} from 'react-router';
+
+const WPLogo = <img src="/WPNavbarLogo.png"/> ;
+
 
 class Navigation extends React.Component {
   constructor(props){
     super(props);
   }
 
+
+  handleTouchTap() {
+  alert('onTouchTap triggered on the component');
+  }
+
+
+
   render() {
+
+    let cartIcon={
+      color: '#F4E04D',
+      fontSize: '45px',
+
+    };
+
+
+
     return (
-        <Navbar inverse collapseOnSelect>
-          <Navbar.Header>
-            <Navbar.Brand>
-              <Link className='brand' to="/">
-                <img src="/WPNavbarLogo.png"/>
-              </Link>
-            </Navbar.Brand>
-            <Navbar.Toggle />
-            <Navbar.Header>
-              { this.props.cart ? this.props.cart.length : ''}
-              <Link to="/cart">Cart</Link>
-            </Navbar.Header>
-          </Navbar.Header>
-          <Navbar.Collapse>
-            <Nav>
-            </Nav>
-            <Nav pullRight>
-              <NavItem eventKey={1} href="#">Login</NavItem>
-              <NavItem eventKey={2} href="#">Signup</NavItem>
-            </Nav>
-          </Navbar.Collapse>
-        </Navbar>
+      <Toolbar>
+
+       <ToolbarTitle text="Wantrepreneur"/>
+
+        <FontIcon className="material-icons" style={cartIcon} hoverColor='#FCFCFC' >shopping_cart</FontIcon>
+
+      </Toolbar>
+
     );
   }
 }
 
 export default Navigation;
+
+
+/* BootStrap NavBar
+<Navbar inverse collapseOnSelect>
+<Navbar.Header>
+<Navbar.Brand>
+<Link className='brand' to="/">
+
+</Link>
+</Navbar.Brand>
+<Navbar.Toggle />
+</Navbar.Header>
+<Navbar.Collapse>
+<Nav>
+</Nav>
+<Nav pullRight>
+<NavItem eventKey={1} >Login</NavItem>
+<NavItem eventKey={2} >Signup</NavItem>
+</Nav>
+</Navbar.Collapse>
+</Navbar>
+*/
