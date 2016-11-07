acf6ee764e18d456dd81d752d179ca360b98140e
Anthony Ardito <anthony.ardito@gmail.com>
2016-11-07 17:38:04 -0500

    adjusted status code for successful login

--- server/auth.js
+++ server/auth.js
@@ -129,7 +129,7 @@ auth.post('/:strategy/login', (req, res, next) => {
     if (!user) { return res.sendStatus(401); }
     req.login(user, err => {
       if (err) { return next(err); }
-      return res.send(user);
+      return res.status(302).send(user);
     });
   })(req, res, next);
 });
