3ac0806a3a062895700d30e2fd5888c50e33169a
Emily H <emilyyho33@gmail.com>
2016-11-07 12:42:10 -0500

    onChange and handlers added for all inputfields

--- app/components/navbar/RegistrationForm.jsx
+++ app/components/navbar/RegistrationForm.jsx
@@ -12,13 +12,61 @@ import {Card, CardHeader, CardActions, CardText} from 'material-ui';
 class RegistrationForm extends React.Component{
   constructor(props){
     super(props);
+
+    this.state = {
+      name : "",
+      lastname: '',
+      email: '',
+      password1 :'',
+      password2 :''
+    };
+
     this.onSubmitSignup = this.onSubmitSignup.bind(this);
+    this.nameHandler =this.nameHandler.bind(this);
+    this.lastnameHandler =this.lastnameHandler.bind(this);
+    this.emailHandler =this.emailHandler .bind(this);
+    this.passwordHandler1 =this.passwordHandler1 .bind(this);
+    this.passwordHandler2 =this.passwordHandler2 .bind(this);
+
+  }
+
+  nameHandler(evt){
+    console.log(evt.target.value);
+    this.setState({
+      name : evt.target.value
+    });
+  }
+  lastnameHandler(evt){
+    console.log(evt.target.value);
+    this.setState({
+      lastName : evt.target.value
+    });
+  }
+  emailHandler(evt){
+    console.log(evt.target.value);
+    this.setState({
+      email : evt.target.value
+    });
+  }
+  passwordHandler1(evt){
+    console.log(evt.target.value);
+    this.setState({
+      password1 : evt.target.value
+    });
+  }
+  passwordHandler2(evt){
+    console.log(evt.target.value);
+    this.setState({
+      password2 : evt.target.value
+    });
   }
 
   onSubmitSignup(event){
     event.preventDefault();
-    console.log(this.props);
+    console.log(this.state);
     console.log(event + "event");
+
+  //  this.props.signUpUser("myUser");
   }
 
 
@@ -41,12 +89,12 @@ class RegistrationForm extends React.Component{
         <CardHeader title="Signup as New User"/>
         <form onSubmit={this.onSubmitSignup}>
         <CardText className="form-group" >
-          <TextField name='firstName' hintText="First Name" /><br/>
-          <TextField name='lastName' hintText="Last Name"/><br/>
-          <TextField name='email' type="email" hintText="Email"  required/><br/>
-          <TextField name='password' hintText="Password" type="password"
+          <TextField name='firstName' label='firstName' hintText="First Name" onChange={this.nameHandler} /><br/>
+          <TextField name='lastName' hintText="Last Name" onChange={this.lastnameHandler}/><br/>
+          <TextField name='email' type="email" hintText="Email" onChange={this.emailHandler} required/><br/>
+          <TextField name='password' hintText="Password" type="password" onChange={this.password2}
            required/><br/>
-          <TextField name='password1' hintText="Confirm Password" type="password"   required/><br/>
+          <TextField name='password1' hintText="Confirm Password" type="password" onChange={this.password2}  required/><br/>
           </CardText>
         <CardActions><RaisedButton type="submit" label="Submit"/></CardActions>
         </form>
