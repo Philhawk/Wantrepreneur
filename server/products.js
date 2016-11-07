82a7543d1abbde1c5ed48c920e8fb1d9a557cf42
Kate Humphrey <khumphrey7890@gmail.com>
2016-11-07 15:17:10 -0500

    logout fixed, admin can get and update orders

--- server/products.js
+++ server/products.js
@@ -5,6 +5,38 @@ const db = require('APP/db');
 
 const productRoutes = require('express').Router();
 
+
+productRoutes.put('/:productId', (req, res, next) => {
+  if (req.user && req.user.roles === 'admin') {
+    db.model('products').update(req.body, {
+    	where: { id: req.params.productId },
+    	returning: true
+    })
+      .then(updatedProduct => {
+        if (updatedProduct) res.sendStatus(204)
+        else res.json("Product does not exist")
+      })
+      .catch(next);
+  } else {
+    next(new ForbiddenError('You must be an admin to access this page.'));
+  }
+});
+
+productRoutes.delete('/:productId', (req, res, next) => {
+  if (req.user && req.user.roles === 'admin') {
+    db.model('products').destroy({
+      where: {id: req.params.productId}
+    })
+      .then(numDeleted => {
+        if (numDeleted) res.sendStatus(204)
+        else res.json("Product does not exist")
+      })
+      .catch(next);
+  } else {
+    next(new ForbiddenError('You must be an admin to access this page.'));
+  }
+});
+
 const products = epilogue.resource({
   model: db.model('products'),
   endpoints: ['/products', '/products/:id'],
