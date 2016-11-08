743c38ffce7ea537887a8c8fd6bfa619d5b35785
Philhawk <pkjacob@gmail.com>
2016-11-08 13:56:50 -0500

    fix sockets

--- server/sockets.js
+++ server/sockets.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const http = require('http');
-var app = require('http').createServer().listen(8080,'127.0.0.1');
+var app = require('http').createServer().listen(8080);
 const io = require('socket.io').listen(app);
 io.sockets.on('connection', (socket) => {
   console.log(`Socket ${socket.id} connected to server.`);
