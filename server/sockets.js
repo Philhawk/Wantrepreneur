43b81df3a5b5773360e361663395aac904070764
Philhawk <pkjacob@gmail.com>
2016-11-08 17:53:00 -0500

    please work ultron

--- server/sockets.js
+++ server/sockets.js
@@ -1,18 +1,14 @@
 'use strict';
 
 const http = require('http');
-var app = require('http').createServer().listen(8080,'127.0.0.1');
-const io = require('socket.io').listen(app);
+var server = require('http').createServer();
+const io = require('socket.io')(server);
 console.log('THIS IS THE IOOOOOOOOO', io)
 io.sockets.on('connection', (socket) => {
   console.log('SOCKETS WORKING')
-  console.log('SOCKETS WORKING')
-  console.log('SOCKETS WORKING')
-  console.log('SOCKETS WORKING')
-  console.log('SOCKETS WORKING')
-  console.log('SOCKETS WORKING')
-  console.log('SOCKETS WORKING')
   console.log(`Socket ${socket.id} connected to server.`);
 });
+
+server.listen(8080)
 
 module.exports = io;
